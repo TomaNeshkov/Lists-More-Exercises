2c350d631243ab39a6b87574374c5fd040a8699e
TomaNeshkov <toma.neshkov.highschool@svetlina.net>
2023-11-26 16:24:48 +0200

    Exercises (100/100)

---
+++ 2. Car Race/2. Car Race/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            double firstCartime = 0.0;
+            double secondCartime = 0.0;
+            for (int i = 0; i < numbers.Length / 2; i++)
+            {
+                firstCartime += numbers[i];
+                if (numbers[i] == 0)
+                {
+                    firstCartime *= 0.8;
+                }
+
+            }
+            for (int i = numbers.Length - 1; i > numbers.Length / 2; i--)
+            {
+                secondCartime += numbers[i];
+                if (numbers[i] == 0)
+                {
+                    secondCartime *= 0.8;
+                }
+
+            }
+        if (firstCartime < secondCartime)
+        {
+            Console.WriteLine($"The winner is left with total time: {Math.Round(firstCartime, 2)}");
+        }
+        else
+        {
+            Console.WriteLine($"The winner is right with total time: {Math.Round(secondCartime, 2)}");
+        }
+
+
+    }
+}
